cebf57e75cf88a01a17776a157dc4ffea2cf91eb
Yeon <nicegod95@gmail.com>
2024-12-10 18:44:38 +0900

    first commit

---
+++ src/main/java/yeon/spring/recruit_project/RecruitProjectApplication.java
@@ -0,0 +1,13 @@
+package yeon.spring.recruit_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/yeon/spring/recruit_project/RecruitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package yeon.spring.recruit_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
